32cc62c1572b0c6a1976f9b77227b8d2aea8c22d
Matyáš Sládek <acc.sladek@gmail.com>
2021-07-25 08:37:04 +0200

    Added database, AutoMapper, Unit of work and DTOs

--- HotelListing/Startup.cs
+++ HotelListing/Startup.cs
@@ -1,7 +1,11 @@
+using AutoMapper;
+using HotelListing.Configurations;
+using HotelListing.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,7 +31,9 @@ namespace HotelListing
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddControllers();
+            services.AddDbContext<DatabaseContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("sqlConnection"))
+            );
 
             services.AddCors(o => {
                 o.AddPolicy("AllowAll", builder =>
@@ -36,10 +42,14 @@ namespace HotelListing
                     .AllowAnyHeader());
             });
 
+            services.AddAutoMapper(typeof(MapperInitializer));
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "HotelListing", Version = "v1" });
             });
+
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
